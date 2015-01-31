2b40598df998b5a6f947b852464447bccaecd349
faraon5593 <faraon5593@gmail.com>
2015-01-31 02:33:18 +0100

    poprawki logowania

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout'}#, sign_up: 'register' }
   resources :produkts
 
    root :to => 'sklep#index'
